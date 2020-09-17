236a10adc71bab228acbe06473e50d47227a6b41
Philipp <philvas13@gmail.com>
2020-09-17 14:01:46 +0300

    Reworked the code

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/public/js/date.js");
 
 const app = express();
 
 const items = ["Do something", "And more", "And more..."];
 
 app.set('view engine', "ejs");
-app.use(bodyParser.urlencoded({   
+app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public")); 
- 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  // const days = ["Sanday", "Monday", "Tuesday", "Wednesfday", "Thursday", "Friday", "Saturday"];
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }; 
-
-  var day = today.toLocaleDateString("en-US", options);
-  console.log(day);
-
-  res.render("list", { 
+  const day = date.getDate();
+  res.render("list", {
     day: day,
     items: items
   });
 });
 
-app.post("/", function (req, res) { 
-  var item = req.body.newItem;
-  if(item !== ""){
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  if (item !== "") {
     items.push(item);
   }
   console.log(items);
